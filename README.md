809935ea6fc21a4fd7dab18f3e1562c4b2016d0e
starmaid <nmasso@protonmail.com>
2026-09-16 16:10:14 -0700

    reformat and spec into docker

--- README.md
+++ README.md
@@ -5,7 +5,20 @@ tools for benchmarking hardware
 
 
 ```
-phoronix-test-suite debug-run build-linux-kernel
-phoronix-test-suite benchmark build-linux-kernel
+docker compose build
 
+on mac and windows, you have to adjust the hardware allocated for docker in docker desktop.
+
+# Install and run tests
+docker compose run --rm pts benchmark build-linux-kernel
+docker compose run --rm pts benchmark ffmpeg
+docker compose run --rm pts benchmark stream
+docker compose run --rm pts benchmark java-scimark2
+
+docker compose run --rm pts list-saved-results
+
+virtualisation.podman.enable = true;
+
+sudo podman build .
+sudo podman compose run --rm pts benchmark java-scimark
 ```
--- flake.nix
+++ flake.nix
@@ -1,48 +1,13 @@
 {
-  description = "An environment for phoronix test suite.";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, ... }:
     let
-      # Change system to "aarch64-linux" if on ARM64
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      # Run directly with `nix run`
-      #apps.${system}.default = {
-      #  type = "app";
-      #  program = "${pkgs.yt-dlp}/bin/yt-dlp";
-      #};
-
-      # Enter shell with `nix develop`
+      pkgs = import nixpkgs { inherit system; };
+    in {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          phoronix-test-suite
-          gcc
-          gnumake
-          bc
-          bison
-          flex
-          openssl
-          pkg-config
-          elfutils
+        packages = [
+          pkgs.podman
         ];
-
-        shellHook = ''
-          unset NIX_LDFLAGS
-          unset NIX_CFLAGS_COMPILE
-          unset NIX_CFLAGS_LINK
-          unset LDFLAGS
-          unset CFLAGS
-          unset CXXFLAGS
-        '';
       };
-      
     };
 }
-
-
